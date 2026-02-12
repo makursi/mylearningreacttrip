ce0c2341b95b3a53d3459d4dad6b6fc5e5bb454f
Choria <2963482580qq.com>
2026-02-12 22:58:01 +0800

    components communication

--- src/components/componentprops.tsx
+++ src/components/componentprops.tsx
@@ -1,6 +1,7 @@
 //组件通行
 //https://message163.github.io/react-docs/react/components/communication.html
 import React from "react";
+import { C } from "vue-router/dist/router-CWoNjPRp.mjs";
 
 //React.FC(function components)是函数式组件, TS使用的一个范型.
 //定义默认值
@@ -126,33 +127,45 @@ import React from "react";
 //--------------------
 
 //兄弟组件通信
-export default function ComponentProps() {
-  const dragonsee = (params: string) => {
-    console.log("帝师说他见过", params);
-  };
-  interface Props {
-    sponsbob: (params: string) => void;
-    children: React.ReactNode;
+//发布-订阅前端架构模式
+declare global {
+  interface Event {
+    params: any;
   }
+}
+const Son: React.FC = (props) => {
+  const event = new Event("on-card"); //添加到事件中心
+
+  const clickTap = () => {
+    console.log(event);
+    event.params = { name: "我见过龙" };
+    window.dispatchEvent(event); //派发事件
+  };
+  return (
+    <>
+      <button onClick={clickTap}>派发事件</button>
+    </>
+  );
+};
+
+const Brother = () => {
+  window.addEventListener("on-card", (event) => {
+    console.log(event.params, "触发了");
+  });
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+export default function ComponentProps() {
   //React.FC<Props> 是 “Function Component” 的 TypeScript 类型别名，全称是 React.FunctionComponent<Props>。
   //子组件接收函数, 在对应的事件中调用函数
-  const Son: React.FC<Props> = (props) => {
-    return (
-      <>
-        <button
-          onClick={() => {
-            props.sponsbob("龙");
-          }}
-        >
-          click
-        </button>
-      </>
-    );
-  };
 
   return (
     <>
-      <Son sponsbob={dragonsee}></Son>
+      <Son></Son>
+      <Brother></Brother>
     </>
   );
 }
