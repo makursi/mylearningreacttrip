e5a95a36e7978c89c1fd87a188f2c47606c7b791
Choria <2963482580qq.com>
2026-02-13 15:38:07 +0800

    components controlled

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import "./App.css";
 //方块组件
 // import Mystatehook from "./hooks/useStateChoria";
-
-import ComponentProps from "./components/componentprops";
+// import ComponentProps from "./components/componentProps";
+import ComponentControlled from "./components/conponentConrtolled";
 function App() {
   return (
     <>
       <h1>练习案例</h1>
       {/* 案例引用 */}
       <div>
-        <ComponentProps></ComponentProps>
+        <ComponentControlled></ComponentControlled>
       </div>
     </>
   );
---
+++ src/components/conponentConrtolled.tsx
@@ -0,0 +1,11 @@
+//react受控组件
+
+const ComponentControlled = () => {
+  return (
+    <>
+      <h1>受控组件</h1>
+    </>
+  );
+};
+
+export default ComponentControlled;
